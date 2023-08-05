2bee821dd7ad54abc53570c25c0f4fc9353c56ec
pchyu <peterch.yu@gmail.com>
2023-08-05 16:04:55 +0800

    4th commit

--- test.py
+++ test.py
@@ -10,3 +10,23 @@ print("hello world!!")
 print("happy new year")
 
 print("Merry Christmas")
+
+# do a bubble sort in a list of 10 random numbers
+import random
+def random_list(n):
+    a = []
+    for i in range(n):
+        a.append(random.randint(0,100))
+    return a
+def bubble(a):
+    for i in range(len(a)):
+        for j in range(len(a)-1):
+            if a[j] > a[j+1]:
+                a[j],a[j+1] = a[j+1],a[j]
+    return a
+ran=random_list(10)
+print(ran)
+print(bubble(ran))
+
+
+
